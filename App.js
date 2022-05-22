2718e14426ce35b92df6c14c4d1382c7ae700950
Kaan Mert <kaan.mert@6hive.co>
2022-05-22 15:30:44 +0300

    Post List Page

--- App.js
+++ App.js
@@ -1,23 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {useState, useEffect} from "react";
 
-export default function App() {
+import { StyleSheet, Text, View, FlatList, SafeAreaView, ActivityIndicator} from "react-native";
+
+const App = () => {
+  const [data, setData] = useState([]);
+  const [isLoading, setLoading] = useState(true)
+//  console.log(data); 
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => console.error(error))
+      .finally(setLoading(false));
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Opsxdgfdgfdggdfsdfsdfdgdrfen up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+        {isLoading ? <ActivityIndicator/> : <FlatList
+          data={data}
+          keyExtractor={({id}, index) => id}
+          renderItem={({item}) =>{
+            if(item.id < 21){
+              return(  
+                <Text>{item.id} : {item.title}</Text>
+              )
+            }
+            else{
+              setLoading(false)
+            }
+           
+          }}
+        />}
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  text : {
+  text: {
     color: "red",
-  }
+  },
 });
+
+export default App;
