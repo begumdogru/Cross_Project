11e808f097057372f178e2de87beb158b07d4189
begumdogru <begum.do@gmail.com>
2022-05-24 18:19:54 +0300

    part1 & part2 - done

--- UserDetail.js
+++ UserDetail.js
@@ -5,21 +5,21 @@ export default function UserDetail({route}) {
     const { itemId, itemName, itemUserName, itemEmail, itemAddress, itemPhone, itemWebsite, itemCompany, itemAddress_Street, itemAddress_Suite, itemAddress_City, itemAddress_Zipcode, itemAddress_Geo_LAT, itemAddress_Geo_LNG,  itemCompany_Name,itemCompany_catchPhrase , itemCompany_Bs } = route.params;
     return (
         <View>
-            <Text>ID: {(itemId)}</Text>
-            <Text>NAME: {(itemName)}</Text>
-            <Text>USERNAME: {(itemUserName)}</Text>
-            <Text>EMAIL: {(itemEmail)}</Text>           
-            <Text>STREET: {(itemAddress_Street)}</Text>
-            <Text>SUITE: {(itemAddress_Suite)}</Text>
-            <Text>ZIPCODE: {(itemAddress_Zipcode)}</Text>
-            <Text>GEO_LAT: {(itemAddress_Geo_LAT)}</Text>
-            <Text>GEO_LAT: {(itemAddress_Geo_LNG)}</Text>
-            <Text>STREET: {(itemAddress_Street)}</Text>
-            <Text>PHONE: {(itemPhone)}</Text>
-            <Text>WEBSITE: {(itemWebsite)}</Text>
-            <Text>COMPANY: {(itemCompany_Name)}</Text>
-            <Text>COMPANY CATCHPHRASE: {(itemCompany_catchPhrase)}</Text>
-            <Text>COMPANY BS: {(itemCompany_Bs)}</Text>
+            <Text style={{ color: "black", fontWeight: "300" }}>ID: {(itemId)}</Text>
+            <Text style={{ color: "black", fontWeight: "300" }}>NAME: {(itemName)}</Text>
+            <Text style={{ color: "black", fontWeight: "300" }}>USERNAME: {(itemUserName)}</Text>
+            <Text style={{ color: "black", fontWeight: "300" }}>EMAIL: {(itemEmail)}</Text>           
+            <Text style={{ color: "black", fontWeight: "300" }}>STREET: {(itemAddress_Street)}</Text>
+            <Text style={{ color: "black", fontWeight: "300" }}>SUITE: {(itemAddress_Suite)}</Text>
+            <Text style={{ color: "black", fontWeight: "300" }}>ZIPCODE: {(itemAddress_Zipcode)}</Text>
+            <Text style={{ color: "black", fontWeight: "300" }}>GEO_LAT: {(itemAddress_Geo_LAT)}</Text>
+            <Text style={{ color: "black", fontWeight: "300" }}>GEO_LAT: {(itemAddress_Geo_LNG)}</Text>
+            <Text style={{ color: "black", fontWeight: "300" }}>STREET: {(itemAddress_Street)}</Text>
+            <Text style={{ color: "black", fontWeight: "300" }}>PHONE: {(itemPhone)}</Text>
+            <Text style={{ color: "black", fontWeight: "300" }}>WEBSITE: {(itemWebsite)}</Text>
+            <Text style={{ color: "black", fontWeight: "300" }}>COMPANY: {(itemCompany_Name)}</Text>
+            <Text style={{ color: "black", fontWeight: "300" }}>COMPANY CATCHPHRASE: {(itemCompany_catchPhrase)}</Text>
+            <Text style={{ color: "black", fontWeight: "300" }}>COMPANY BS: {(itemCompany_Bs)}</Text>
 
         </View>
     )
